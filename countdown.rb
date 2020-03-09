a9807a10208124f9370651e635805ad38bd34863
Philippe Timothe <philippetimothe@gmail.com>
2020-03-09 21:53:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,12 +7,12 @@ def countdown(number)
   puts "#{number} SECOND(S)!"
   number -= 1
   end
-  
+  puts "HAPPY NEW YEAR!"
   def countdown_with_sleep(number)
   sleep(number)
   end
   
-  puts "HAPPY NEW YEAR!"
+  
 end
 
 
